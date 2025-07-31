4e5cde584d0051fa6312b779f55635045c052944
Amar kumar <130559952+AmarKumar255@users.noreply.github.com>
2025-08-01 01:20:34 +0530

    Update Test5Application.java

--- test5/src/main/java/com/test5/Test5Application.java
+++ test5/src/main/java/com/test5/Test5Application.java
@@ -9,6 +9,7 @@ public class Test5Application {
 	public static void main(String[] args) {
   		string name="mike";
                 int x=100;
+		int y=200;
 		SpringApplication.run(Test5Application.class, args);
 	}
 
